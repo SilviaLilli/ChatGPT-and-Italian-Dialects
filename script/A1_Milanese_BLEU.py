e5c3a1216b41c6f3454f858acea361679b5f1fb2
SilviaLilli <145535639+SilviaLilli@users.noreply.github.com>
2023-09-21 15:16:17 +0200

    Update A1_Milanese_BLEU.py

--- script/A1_Milanese_BLEU.py
+++ script/A1_Milanese_BLEU.py
@@ -1,39 +1,40 @@
+import requests
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
-with open (r"https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt", "r") as ref:
-    with open (r"https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_DefTemp.txt", "r") as hyp:
-        
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_DefTemp.txt"
 
-        reference = ref.read().split()
-        hypotesis = hyp.read().split()
-        smooth = SmoothingFunction().method7
-        score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
-        print(score)
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
+      
 
-        
-
-
-with open (r"https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt", "r") as ref:
-    with open (r"https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_LowTemp.txt", "r") as hyp:
-        
-
-        reference = ref.read().split()
-        hypotesis = hyp.read().split()
-        smooth = SmoothingFunction().method7
-        score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
-        print(score)
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+smooth = SmoothingFunction().method7
+score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
+print(score)
 
         
 
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_LowTemp.txt"
 
-with open (r"https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt", "r") as ref:
-    with open (r"https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_LowTemp.txt", "r") as hyp:
-        
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
 
-        reference = ref.read().split()
-        hypotesis = hyp.read().split()
-        smooth = SmoothingFunction().method7
-        score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
-        print(score)
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+smooth = SmoothingFunction().method7
+score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
+print(score)
 
 
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_HiTemp.txt"
+
+
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+smooth = SmoothingFunction().method7
+score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
+print(score)
