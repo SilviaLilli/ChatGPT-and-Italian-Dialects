e5f88604daa2df1bfbfbbccf0dc8dbc47f764323
SilviaLilli <145535639+SilviaLilli@users.noreply.github.com>
2023-09-21 17:42:56 +0200

    Create C2_Milanese_TER.py

---
+++ scripts/C2_Milanese_TER.py
@@ -0,0 +1,29 @@
+import requests
+import pyter
+
+#My traslation as source
+
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_El%20Princip%20Piscinin.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/ITA%20to%20Milanese_ChatGPTs%20Translation_fromMyTransl.txt"
+
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
+
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+score = pyter.ter(hypotesis, reference)
+print(score)
+
+#Brigoli's translation as source
+
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_El%20Princip%20Piscinin.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/ITA%20to%20Milanese_ChatGPTs%20Translation_fromBrigoli.txt"
+
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
+
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+score = pyter.ter(hypotesis, reference)
+print(score)
+
