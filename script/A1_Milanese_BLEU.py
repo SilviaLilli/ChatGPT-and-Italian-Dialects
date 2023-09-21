82c8d01f05eac1bcbedc960c69b66765595bb713
SilviaLilli <145535639+SilviaLilli@users.noreply.github.com>
2023-09-21 14:36:14 +0200

    Create A1_Milanese_BLEU.py

---
+++ script/A1_Milanese_BLEU.py
@@ -0,0 +1,39 @@
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+
+with open (r"", "r") as ref:
+    with open (r"", "r") as hyp:
+        
+
+        reference = ref.read().split()
+        hypotesis = hyp.read().split()
+        smooth = SmoothingFunction().method7
+        score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
+        print(score)
+
+        
+
+
+with open (r"", "r") as ref:
+    with open (r"", "r") as hyp:
+        
+
+        reference = ref.read().split()
+        hypotesis = hyp.read().split()
+        smooth = SmoothingFunction().method7
+        score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
+        print(score)
+
+        
+
+
+with open (r"", "r") as ref:
+    with open (r"", "r") as hyp:
+        
+
+        reference = ref.read().split()
+        hypotesis = hyp.read().split()
+        smooth = SmoothingFunction().method7
+        score = sentence_bleu([reference], hypotesis, smoothing_function=smooth)
+        print(score)
+
+
