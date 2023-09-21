09adabd6072859823297759e1c6d707d9141534d
SilviaLilli <145535639+SilviaLilli@users.noreply.github.com>
2023-09-21 15:48:35 +0200

    Create A1_Milanese_TER.py

---
+++ script/A1_Milanese_TER.py
@@ -0,0 +1,41 @@
+import requests
+import pyter
+
+#Default Temperature Setting
+
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_DefTemp.txt"
+
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
+
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+score = pyter.ter(hypotesis, reference)
+print(score)
+
+#Low Temperature Setting
+
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_LowTemp.txt"
+
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
+
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+score = pyter.ter(hypotesis, reference)
+print(score)
+
+#High Temperature Setting
+
+url_ref = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese%20to%20Italian_mytranslation.txt"
+url_hyp = "https://raw.githubusercontent.com/SilviaLilli/ChatGPT-and-Italian-Dialects/main/data/texts/Milanese_ChatGPTs%20Translation_HiTemp.txt"
+
+response_ref = requests.get(url_ref)
+response_hyp = requests.get(url_hyp)
+
+reference = response_ref.text.split()
+hypotesis = response_hyp.text.split()
+score = pyter.ter(hypotesis, reference)
+print(score)
